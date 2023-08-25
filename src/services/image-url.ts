4230cc8af038fb5e609c265d381ee472ddd73ac8
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-08-25 23:19:06 +0300

    fixing bugs with low resolution images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,15 @@
 const getCroppedImageUrl = (url: string) => {
-  const index = url.indexOf("media/") + "media/".length;
-  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  const img = new Image();
+
+  img.onload = function () {
+    if (img.width <= 600 || img.height <= 400) {
+    } else {
+      const index = url.indexOf("media/") + "media/".length;
+      return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+    }
+  };
+  img.src = url;
+  return url;
 };
 
 export default getCroppedImageUrl;
